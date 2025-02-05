0598d1b4ed81a6420c21e55ab209cb9dfa0313dc
elena-louka <louka.elen@gmail.com>
2025-02-05 20:46:45 +0200

    set up react + tailwind app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold underline">Tailwind is Working! 🚀</h1>
+      <p className="mt-4 text-lg text-pink-400">If you see this styled, Tailwind is running!</p>
+      <button className="mt-6 px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-700">
+        Click Me!
+      </button>
+    </div>
+  );
+}
+
+export default App;
